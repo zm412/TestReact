53c9b792e656dee659f365bfeffb824dba8a8312
zm412 <zm412412@gmail.com>
2020-12-10 15:55:32 -0500

    started create filters

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,21 +14,52 @@ export default function Home() {
   const [users, setUsers] = useState(JSON.parse(localStorage.getItem('users')))
   const [changeUser, setChangeUser] = useState(false);
   const [newItem, setNewItem] = useState(false);
+  const [id, setId] = useState('');
   const [email, setEmail] = useState('');
   const [name, setName] = useState('');
   const [phoneNumber, setPhoneNumber] = useState('');
   const [status, setStatus] = useState('');
   const [created, setCreated] = useState('');
   const [updated, setUpdated] = useState('');
+  const [searchEmail, setSearchEmail] = useState('');
+  const [searchPhone, setSearchPhone] = useState('');
   const [isUpdating, setIsUpdating] = useState(-1);
+  const [changed, setChanged] = useState(-1);
+  const [isCompressed, setIsCompressed] = useState(false);
 
   const classes = useStyles();
+
+  const currentPos = (id, arr) => {
+    for( let i = 0; i < arr.length; i++ ){
+      if(id == arr[i].id) return i
+    }
+  }
+
+  const searchByEmail = () => {
+    setIsCompressed(true);
+    let store = JSON.parse(localStorage.getItem('users'));
+    let newStore = store.filter(item => item.email == searchEmail);
+    setUsers(newStore)
+  }
+
+  const searchByPhone = () => {
+    let store = JSON.parse(localStorage.getItem('users'));
+    store.filter(item => item.phoneNumber == searchPhone);
+    setUsers(store)
+  }
+
+  const removeFilter = () => {
+    let store = JSON.parse(localStorage.getItem('users'));
+    setUsers(store)
+    setIsCompressed(false)
+  }
 
 
 
   const saveChanges = () => {
     let store = JSON.parse(localStorage.getItem('users'));
-    store.push({email: email, phoneNumber: phoneNumber, name: name, status: status, created: created, updated: updated});
+    let newId = store[store.length - 1].id + 1;
+    store.push({id: newId,email: email, phoneNumber: phoneNumber, name: name, status: status, created: created, updated: updated});
     localStorage.setItem('users', JSON.stringify(store));
     setChangeUser(true)
     setNewItem(false)
@@ -54,43 +85,42 @@ export default function Home() {
   const deleteRow = (e) => {
     e.preventDefault();
     let parent = e.target.closest('.row');
-    let pos = parent.firstElementChild.innerHTML - 1;
+    let id = parent.firstElementChild.innerHTML;
     let store = JSON.parse(localStorage.getItem('users'));
-    store.splice(pos, 1);
-    localStorage.setItem('users', JSON.stringify(store));
+    let newStore = store.filter(item => item.id != id)
+    localStorage.setItem('users', JSON.stringify(newStore));
     setChangeUser(true);
   }
 
-  const cancelUpdate = () => {
-    setIsUpdating(-1);
+  const updateRow = (e) => {
+    e.preventDefault();
+    let parent = e.target.closest('.row');
+    let id = parent.firstElementChild.innerHTML;
+    let posLocal = currentPos(id, users);
+    let store = JSON.parse(localStorage.getItem('users'));
+    let posGlobal = currentPos(id, store);
+    rememberState(posGlobal)
+    setIsUpdating(posGlobal);
+    setChanged(posLocal);
   }
+
+  const cancelUpdate = () =>  setIsUpdating(-1)
 
 
   const rememberState = (pos) => {
     let store = JSON.parse(localStorage.getItem('users'));
 
+      setId(store[pos].id);
       setEmail(store[pos].email);
       setPhoneNumber(store[pos].phoneNumber);
       setName(store[pos].name);
       setCreated(store[pos].created);
       setUpdated(store[pos].updated);
-    console.log(email,  phoneNumber,name, status)
   }
 
-  const updateRow = (e) => {
-    setChangeUser(true)
-    e.preventDefault();
-    let parent = e.target.closest('.row');
-    let pos = parent.firstElementChild.innerHTML - 1;
-    rememberState(pos)
-    setIsUpdating(pos);
-  }
-
-  const saveUpdate = async () => {
-    await rememberState(isUpdating);
-    console.log(email,  phoneNumber,name, status)
+  const saveUpdate = () => {
     let store = JSON.parse(localStorage.getItem('users'));
-    store[isUpdating] = {email: email, phoneNumber: phoneNumber, name: name, status: status, created: created, updated: updated}
+    store[isUpdating] = {id: id, email: email, phoneNumber: phoneNumber, name: name, status: status, created: created, updated: updated}
     localStorage.setItem('users', JSON.stringify(store));
     console.log(isUpdating)
     setIsUpdating(-1);
@@ -109,11 +139,12 @@ export default function Home() {
     eval(key);
   }
 
+
    
   let tabBody = users.map((row, index) => (
     index != isUpdating ?
             <TableRow key={index} className='row'>
-              <TableCell align="right">{index+1}</TableCell>
+              <TableCell align="right">{row.id}</TableCell>
               <TableCell align="right"> {row.email} </TableCell>
               <TableCell align="right">{row.phoneNumber}</TableCell>
               <TableCell align="right">{row.name}</TableCell>
@@ -125,7 +156,7 @@ export default function Home() {
             </TableRow>
     :
       <TableRow>
-        <TableCell align="right">{index+1}</TableCell>
+        <TableCell align="right">{row.id}</TableCell>
         <TableCell align="right" ><TextField id='Email' defaultValue={row.email} onChange={onChangeValue}/></TableCell>
         <TableCell align="right"><TextField id='PhoneNumber'  defaultValue={row.phoneNumber} onChange={onChangeValue} /></TableCell>
         <TableCell align="right" ><TextField id='Name' defaultValue={row.name} onChange={onChangeValue}/></TableCell>
@@ -143,7 +174,7 @@ export default function Home() {
         <TableCell align="right"><Button variant='outlined' onClick={cancelUpdate}>Cancel</Button></TableCell>
         </TableRow>
  
-          ))
+          ));
 
   return (
     <div>
@@ -163,10 +194,29 @@ export default function Home() {
           </TableRow>
         </TableHead>
         <TableBody>
+          <TableRow>
+            <TableCell align="right"></TableCell>
+            <TableCell align="right" ><TextField id='SearchEmail' onChange={onChangeValue}/><Button onClick={searchByEmail} variant='outlined'>Search</Button></TableCell>
+            <TableCell align="right"><TextField id='SearchPhone' onChange={onChangeValue} /><Button onClick={searchByPhone} variant='outlined'>Search</Button></TableCell>
+            <TableCell align="right"></TableCell>
+            <TableCell align="right" >
+                <Select defaultValue='client' name='Status' onChange={onChangeValue} >
+                  <MenuItem value='client'>Client</MenuItem>
+                  <MenuItem value='partner'>Partner</MenuItem>
+                  <MenuItem value='admin'>Admin</MenuItem>
+                </Select>
+            </TableCell>
+
+
+            <TableCell align="right"></TableCell>
+            <TableCell align="right"></TableCell>
+            <TableCell align="right"></TableCell>
+            <TableCell align="right"></TableCell>
+          </TableRow>
           {tabBody}
     
     {newItem && (
-      <TableRow>
+      <TableRow className='row'>
               <TableCell align="right" ></TableCell>
               <TableCell align="right" ><TextField id='Email' onChange={onChangeValue}/></TableCell>
               <TableCell align="right"><TextField id='PhoneNumber' onChange={onChangeValue} /></TableCell>
@@ -202,6 +252,12 @@ export default function Home() {
           <Button variant='contained' onClick={saveChanges}>Save new item</Button>
           <Button variant='contained' onClick={closeAddItem}>Cancel</Button>
         </div>
+      )
+    }
+
+    {
+      isCompressed && (
+        <Button variant='outlined' onClick={removeFilter}>Remove filter </Button>
       )
     }
     </div>
