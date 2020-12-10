4e33c0f34eaa7f9d4b6d0422724cc99f00846577
zm412 <zm412412@gmail.com>
2020-12-09 19:46:47 -0500

    added table

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
-import React from 'react'
-import {Container,Paper,Table, TableContainer, TableHead, TableRow, TableBody, TableCell, Button, Grid, Typography, Breadcrumbs} from  '@material-ui/core';
+import React from 'react';
+import {useState, useEffect} from 'react';
+import {TextField, Container,Paper,Table, TableContainer, TableHead, TableRow, TableBody, TableCell, Button, Grid, Typography, Breadcrumbs} from  '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles({
@@ -10,44 +11,59 @@ const useStyles = makeStyles({
 
 export default function Home() {
 
+  const [users, setUsers] = useState(JSON.parse(localStorage.getItem('users')))
+  const [newUser, setNewUser] = useState(false);
+  const [newItem, setNewItem] = useState(false);
+  const [email, setEmail] = useState('');
+  const [name, setName] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [status, setStatus] = useState('');
+  const [created, setCreated] = useState(null);
+  const [updated, setUpdated] = useState(null);
+
   const classes = useStyles();
 
-  localStorage.setItem('users', JSON.stringify([{ email: 'email', phoneNumber: 'phoneNumber', name: 'name', status: 'status', created: 'createdData', updated: 'updatedData', redactItem: 'Redact', updateItem: 'Update' , deleteItem: 'Delete'}, {email: 1, phoneNumber: 2,name: 3,status: 4, created: new Date(), updated: new Date(),redactItem: 'buttonRedact', updateItem: 'buttonUpdate',deleteItem: 'buttonDelete'}]))  ;
 
-  const getUsers = () => {
-    return JSON.parse(localStorage.getItem('users'));
+
+  const saveChanges = () => {
+    let store = JSON.parse(localStorage.getItem('users'));
+    store.push({email: email, name: name, phoneNumber: phoneNumber, created: created, updated: updated});
+    localStorage.setItem('users', JSON.stringify(store));
+    setNewUser(true)
+    setNewItem(false)
   }
 
-  let style = { table: {
-    border: '1px solid black'
-  } }
+  useEffect (() => {
+    setUsers(JSON.parse(localStorage.getItem('users')))
+  }, [newUser])
 
-  let users = getUsers();
+
+  const addItem = () =>  setNewItem(true);
+  const closeAddItem = () => setNewItem(false);
 
    
 
   return (
+    <div>
     <TableContainer component={Paper}>
       <Table className={classes.table} size="small" aria-label="a dense table">
         <TableHead>
           <TableRow>
-            <TableCell>Email</TableCell>
-            <TableCell align="right">Phone number</TableCell>
-            <TableCell align="right">Name</TableCell>
-            <TableCell align="right">Status</TableCell>
-            <TableCell align="right">Created</TableCell>
-            <TableCell align="right">Updated</TableCell>
-            <TableCell align="right">Redact</TableCell>
-            <TableCell align="right">Update</TableCell>
-            <TableCell align="right">Delete</TableCell>
+            <TableCell component="th" scope="row" align="right">Email</TableCell>
+            <TableCell  component="th" scope="row" align="right">Phone number</TableCell>
+            <TableCell  component="th" scope="row" align="right">Name</TableCell>
+            <TableCell  component="th" scope="row" align="right">Status</TableCell>
+            <TableCell  component="th" scope="row" align="right">Created</TableCell>
+            <TableCell  component="th" scope="row" align="right">Updated</TableCell>
+            <TableCell  component="th" scope="row" align="right">Redact</TableCell>
+            <TableCell  component="th" scope="row" align="right">Update</TableCell>
+            <TableCell  component="th" scope="row" align="right">Delete</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {users.map((row, index) => (
             <TableRow key={row.index}>
-              <TableCell component="th" scope="row">
-                {row.email}
-              </TableCell>
+              <TableCell align="right"> {row.email} </TableCell>
               <TableCell align="right">{row.phoneNumber}</TableCell>
               <TableCell align="right">{row.name}</TableCell>
               <TableCell align="right">{row.status}</TableCell>
@@ -58,8 +74,40 @@ export default function Home() {
               <TableCell align="right">{row.deleteItem}</TableCell>
             </TableRow>
           ))}
+    
+    {newItem && (
+      <TableRow>
+              <TableCell align="right" ><TextField onChange={(e)=>setEmail(e.target.value)}/></TableCell>
+              <TableCell align="right" ><TextField onChange={(e)=>setName(e.target.value)}/></TableCell>
+              <TableCell align="right" ><TextField onChange={(e)=>setStatus(e.target.value)}/></TableCell>
+              <TableCell align="right"><TextField onChange={(e)=>setPhoneNumber(e.target.value)} /></TableCell>
+              <TableCell align="right"><TextField onChange={(e)=>setCreated(new Date())}/></TableCell>
+              <TableCell align="right"><TextField onChange={(e)=>setUpdated(new Date())}/></TableCell>
+              <TableCell align="right"><Button variant='contained'>Redact</Button></TableCell>
+              <TableCell align="right"><Button variant='contained'>Update</Button></TableCell>
+              <TableCell align="right"><Button variant='contained'>Delete</Button></TableCell>
+            </TableRow>
+          )}
+    
+
+
         </TableBody>
       </Table>
     </TableContainer>
+
+    { !newItem && (
+      <Button variant='contained' onClick={addItem}>Add item</Button>
+    ) }
+
+    {
+      newItem && (
+        <div>
+          <Button variant='contained' onClick={saveChanges}>Save changes</Button>
+          <Button variant='contained' onClick={closeAddItem}>Cancel</Button>
+        </div>
+      )
+    }
+    </div>
+    
   )
 }
