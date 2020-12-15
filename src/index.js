6a987ba473ce6b31cc27fdb37061cebd532b7603
zm412 <zm412412@gmail.com>
2020-12-15 14:19:39 -0500

    started change for redux

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
 import { render } from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import App from './components/App';
+import store from './store/configureStore.js';
 
 render((
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 ), document.getElementById('root'));
